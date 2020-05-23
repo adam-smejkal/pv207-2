407fe423777b165354d5b69f0a50ab178f98c1e6
wbadmin <>
2020-05-23 18:19:16 +0200

    {/src/main/java/com/muni/seminarorganizer/Seminar.java}

--- src/main/java/com/muni/seminarorganizer/Seminar.java
+++ src/main/java/com/muni/seminarorganizer/Seminar.java
@@ -6,12 +6,24 @@ package com.muni.seminarorganizer;
 
 public class Seminar implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Seminar() {
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
 
+	public Seminar() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Seminar(java.lang.String name) {
+		this.name = name;
+	}
 
 }
